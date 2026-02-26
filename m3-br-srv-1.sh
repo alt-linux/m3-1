dab7d10c1e046360365b75e0bd7e436c8d1d0a18
alt-linux <amogyous@gmail.com>
2026-02-26 19:48:47 +0300

    Add files via upload

--- m3-br-srv-1.sh
+++ m3-br-srv-1.sh
@@ -1,4 +1,7 @@
-echo "mount /dev/sr0 /mnt/"
-echo "cp /down/import ./"
-echo "chmod +x ./import"
-echo "bash ./import"
+echo 'echo "nameserver 192.168.0.1" > /etc/resolv.conf'
+echo "mount /dev/sr0 /mnt/ && cp /down/import ./ && chmod +x ./import && bash ./import"
+echo "Zahod B ADMC na hq-cli"
+echo "Login:Administrator Password:P@ssw0rd"
+echo "Esli ne zahod, idi v terminal pishi:"
+echo "kinit Administrator"
+echo "zatem izmeni password na P@ssw0rd1 esli zaprosit"
